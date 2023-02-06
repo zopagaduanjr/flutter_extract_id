34f9785d115f3da889443f69517dab62191cb8c5
Zaldy Pagaduan <zaldypagaduanjr@gmail.com>
2023-02-06 18:40:54 +0800

    init files

---
+++ android/app/src/main/kotlin/com/zalboi/flutter_extract_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zalboi.flutter_extract_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/extract_id_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExtractIDScreen extends StatefulWidget {
+  const ExtractIDScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExtractIDScreen> createState() => _ExtractIDScreenState();
+}
+
+class _ExtractIDScreenState extends State<ExtractIDScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
